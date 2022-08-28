d309b735ab867eca74483e08d4c93a94694a320f
siddhu-1002 <65851990+siddhu-1002@users.noreply.github.com>
2022-08-28 16:23:40 +0530

    Add files via upload

---
+++ nagarro/training/assignment1/insurancePremiumCalculation/CalculateInsurance.java
@@ -0,0 +1,81 @@
+package com.nagarro.training.assignment1.insurancePremiumCalculation;
+
+public class CalculateInsurance {
+
+    private String model;
+    private String type;
+    private double price;
+    private String insuranceType;
+
+    //    private String[] carTypes = {"Hatchback", "Sedan", "SUV"};
+    private float[] insuranceAmount = {0.05F, 0.08F, 0.10F};
+    private String[] insuranceTypes = {"Basic", "Premium"};
+
+    public CalculateInsurance(String model, String type, int price,
+                              String insuranceType) {
+        this.model = model;
+        this.type = type;
+        this.price = price;
+        this.insuranceType = insuranceType;
+    }
+
+    // Getters and Setters to access the data or modify it
+    public String getModel() {
+        return this.model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getType() {
+        return this.type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public double getPrice() {
+        return this.price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getInsuranceType() {
+        return this.insuranceType;
+    }
+
+    public void setInsuranceType(String insuranceType) {
+        this.insuranceType = insuranceType;
+    }
+
+
+    protected double calculateInsuranceAmount() {
+
+        double amount = 0;
+
+        if (insuranceType.equals("Basic")) {
+            switch (type) {
+                case "Hatchback":
+                    amount += (price * insuranceAmount[0]);
+                    break;
+                case "Sedan":
+                    amount += (price * insuranceAmount[1]);
+                    break;
+                case "Suv":
+                    amount += (price * insuranceAmount[2]);
+                    break;
+
+            }
+        }
+
+        else if (insuranceType.equals("Premium")){
+            amount += (price * 0.2);
+            }
+
+        return price + amount;
+    }
+}
